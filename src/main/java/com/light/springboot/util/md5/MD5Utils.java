49d01d79e2cfee1b5ccc5be1eb96b343868fbf84
zzli <lizhenzh@yuanian.com>
2019-08-12 09:14:35 +0800

    添加用户以及密码模块等功能

---
+++ src/main/java/com/light/springboot/util/md5/MD5Utils.java
@@ -0,0 +1,38 @@
+package com.light.springboot.util.md5;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author 李振振
+ * @version 1.0
+ * @date 2019/8/10 17:52
+ */
+public class MD5Utils {
+
+    public static String genInitPassWord(){
+        String initPassWord = "123456";
+        return md5Encrypt32Lower(initPassWord);
+    }
+
+    public static String md5Encrypt32Lower(String string) {
+        byte[] hash;
+        try {
+            //创建一个MD5算法对象，并获得MD5字节数组,16*8=128位
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("Huh, UTF-8 should be supported?", e);
+        }
+
+        //转换为十六进制字符串
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString().toLowerCase();
+    }
+}
--- src/main/webapp/pages/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-${user}___${user.age}
-</body>
-</html>
