56bb12a1366ad70f43ca2930c91864914b19ee59
lzz <18366271217@163.com>
2019-10-31 15:11:30 +0800

    优化定时任务配置，线程池中跑定时任务，优化池数量为20

--- src/main/java/com/light/springboot/util/task/ScheduleTask.java
+++ src/main/java/com/light/springboot/util/task/ScheduleTask.java
@@ -3,26 +3,30 @@ package com.light.springboot.util.task;
 import com.alibaba.fastjson.JSON;
 import com.light.springboot.domain.email.Email;
 import com.light.springboot.service.common.EmailService;
+import com.light.springboot.util.bean.SpringContextUtil;
 import com.light.springboot.util.email.EmailUtil;
 import com.light.springboot.util.log.LogUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * 实现了DisposableBean,当spring容器关闭时，自动去关闭scheduler对应的线程池
  * @author 李振振
  * @version 1.0
  * @date 2019/10/29 16:53
  */
 @Component
 @Slf4j
-public class ScheduleTask {
+public class ScheduleTask implements DisposableBean {
     @Autowired
     private EmailService emailService;
     /**
@@ -30,14 +34,14 @@ public class ScheduleTask {
      * 定义每5秒执行该任务
      */
     @Async
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 60*1000)
     public void sendEmailScheduled(){
         LogUtil.info("邮件定时发送开始");
         List<Email> sendList =   emailService.syncMsg();
         sendList.forEach(email -> {
-            EmailUtil emailUtil = EmailUtil.getInstance(email.getEmailConfig().getSender(), email.getEmailConfig().getPassWord(),
-                    (List<String>)JSON.parseObject(email.getReceiver(),List.class),
-                    email.getCopyReceiver()==null?null:(List<String>)JSON.parseObject(email.getCopyReceiver(),List.class), email.getContent(), email.getSubject());
+            EmailUtil emailUtil = EmailUtil.getInstance(email.getEmailConfig().getHost(),email.getEmailConfig().getSender(), email.getEmailConfig().getPassWord(),
+                    JSON.parseArray(email.getReceiver(),String.class), JSON.parseArray(email.getCopyReceiver(),String.class)
+                    , email.getContent(), email.getSubject());
             // 发件邮箱开启了授权码验证，所以这里的密码就是授权码（haolz9014）,原密码为"apurelove9014"
             try {
                 emailUtil.sendEmail(emailUtil);
@@ -47,5 +51,16 @@ public class ScheduleTask {
                 e.printStackTrace();
             }
         });
+    }
+
+    /**
+     * 执行销毁方法
+     * @throws Exception
+     */
+    @Override
+    public void destroy() throws Exception {
+        //关闭线程或线程池
+        ThreadPoolTaskScheduler scheduler = (ThreadPoolTaskScheduler) SpringContextUtil.getBean("scheduler");
+        scheduler.shutdown();
     }
 }
